15196a7a1d9f77b042f771c514c1553eb1bba570
officialkkyshimla <kkyshimlatechguy@gmail.com>
2026-09-13 22:40:39 +0530

    Wrong dependency selected so New App create with Right dependency

--- CircuitBreaker-Cloud-Native_E-Commerce_API_Gateway/backend/src/test/java/Server/backend/BackendApplicationTests.java
+++ CircuitBreaker-Cloud-Native_E-Commerce_API_Gateway/backend/src/test/java/Server/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.server.Server;
+package Server.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
